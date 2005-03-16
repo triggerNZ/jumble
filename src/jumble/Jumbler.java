102b4c855656bb92c3a1276938ca68010e057753
tp20 <tp20@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2005-03-16 09:21:41 +0000

    Removed the dependency on CLIFlags.java and used Weka's command line methods instead.
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@8 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- src/jumble/Jumbler.java
+++ src/jumble/Jumbler.java
@@ -2,9 +2,8 @@ package jumble;
 
 import org.apache.bcel.classfile.JavaClass;
 import java.util.HashSet;
-import com.reeltwo.util.CLIFlags.Validator;
-import com.reeltwo.util.CLIFlags;
-import com.reeltwo.util.CLIFlags.Flag;
+
+import weka.core.Utils;
 
 /**
  * Mutation tester.  Given a class file can either count the number of
@@ -16,110 +15,124 @@ import com.reeltwo.util.CLIFlags.Flag;
  * @version $Revision$
  */
 public class Jumbler extends org.apache.bcel.util.ClassLoader {
-
-  /** Target class for mutation. */
-  private final String mTarget;
-  /** Mutation engine. */
-  private final Mutater mMutater;
-
-  public String getModification() {
-    return mMutater.getModification();
-  }
-  
-  /**
-   * Construct a new loader which will induce a single point mutation
-   * in the target.
-   *
-   * @param target class name to undergo mutation
-   * @param mutater mutation engine
-   */
-  protected Jumbler(final String target, final Mutater mutater) {
-    super();
-    mTarget = target;
-    mMutater = mutater;
-  }
-
-  /**
-   * If the class matches the target then it is mutated, otherwise
-   * the class if returned unmodified.  Overrides the corresponding
-   * method in the superclass.
-   *
-   * @param clazz modification target
-   * @return possibly modified class
-   */
-  protected JavaClass modifyClass(final JavaClass clazz) {
-    //    System.err.println("Mod " + clazz.getClassName());
-    if (clazz.getClassName().equals(mTarget)) {
-      try {
-        return mMutater.jumbler(clazz.getClassName());
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
+    
+    /** Target class for mutation. */
+    private final String mTarget;
+    /** Mutation engine. */
+    private final Mutater mMutater;
+    
+    public String getModification() {
+	return mMutater.getModification();
     }
-    return clazz;
-  }
-
-  /**
-   * Process each of the supplied files in turn. Reports problems with
-   * variable names.
-   *
-   * @param args array of java file names
-   */
-  public static void main(String[] args) {
-
-    // set up and parse command line arguments
-    final CLIFlags flags = new CLIFlags("Jumbler", "Mutation testing tool.");
-    flags.registerOptional('c', "count", "Count possible mutation points");
-    flags.registerOptional('x', "exclude", String.class, "[method[,]]+", "Comma separated list of method names to ignore");
-    flags.registerOptional('k', "inlineconstants", "Allow mutation of inline constants");
-    flags.registerOptional('r', "returns", "Allow mutation of return values");
-    flags.registerRequired(String.class, "class", "Class to be mutated");
-    final Flag a1 = flags.registerRequired(Integer.class, "mutation-point", "point to mutate");
-    final Flag a2 = flags.registerRequired(String.class, "test-class", "corresponding test class");
-    a1.setMinCount(0);
-    a2.setMinCount(0);
-    flags.setValidator(new Validator() {
-        public boolean isValid(CLIFlags flags) {
-          if (flags.getAnonymousValue(1) != null && flags.getAnonymousValue(2) == null) {
-            flags.setParseMessage("must give both mutation point and test class");
-            return false;
-          }
-          return true;
-        }
-      });
-    // following call will cause exit if problem occurs in parsing options
-    flags.setFlags(args);
-
-    // ignore set
-    final HashSet ignore = new HashSet();
-    if (flags.isSet("exclude")) {
-      final String[] ig = ((String) flags.getValue("exclude")).split(",");
-      for (int i = 0; i < ig.length; i++) {
-        ignore.add(ig[i]);
-      }
-    } else {
-      ignore.add("main");
-      ignore.add("integrity");
+    
+    /**
+     * Construct a new loader which will induce a single point mutation
+     * in the target.
+     *
+     * @param target class name to undergo mutation
+     * @param mutater mutation engine
+     */
+    protected Jumbler(final String target, final Mutater mutater) {
+	super();
+	mTarget = target;
+	mMutater = mutater;
     }
-
-    if (flags.isSet("count")) {
-      final Mutater m = new Mutater(0); // run in count mode only, param ignored
-      m.setIgnoredMethods(ignore);
-      m.setMutateInlineConstants(flags.isSet("inlineconstants"));
-      m.setMutateReturnValues(flags.isSet("returns"));
-      System.out.println("" + m.countMutationPoints((String) flags.getAnonymousValue(0)));
-    } else {
-      try {
-        final Mutater m = new Mutater(((Integer) flags.getAnonymousValue(1)).intValue());
-        m.setIgnoredMethods(ignore);
-        m.setMutateInlineConstants(flags.isSet("inlineconstants"));
-        m.setMutateReturnValues(flags.isSet("returns"));
-        final ClassLoader loader = new Jumbler(((String) flags.getAnonymousValue(0)).replace('/', '.'), m);
-        final Class clazz = loader.loadClass("jumble.JumbleTestSuite");
-        System.err.println(clazz.getMethod("run", new Class[] { String.class }).invoke(null, new Object[] { ((String) flags.getAnonymousValue(2)).replace('/', '.') }));
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
+    
+    /**
+     * If the class matches the target then it is mutated, otherwise
+     * the class if returned unmodified.  Overrides the corresponding
+     * method in the superclass.
+     *
+     * @param clazz modification target
+     * @return possibly modified class
+     */
+    protected JavaClass modifyClass(final JavaClass clazz) {
+	//    System.err.println("Mod " + clazz.getClassName());
+	if (clazz.getClassName().equals(mTarget)) {
+	    try {
+		return mMutater.jumbler(clazz.getClassName());
+	    } catch (Exception e) {
+		e.printStackTrace();
+	    }
+	}
+	return clazz;
     }
-  }
+    
+    /**
+     * Process each of the supplied files in turn. Reports problems with
+     * variable names.
+     *
+     * @param args array of java file names
+     */
+    public static void main(String[] args) throws Exception {
+	
+	/*
+	  // set up and parse command line arguments
+	  final CLIFlags flags = new CLIFlags("Jumbler", "Mutation testing tool.");
+	  flags.registerOptional('c', "count", "Count possible mutation points");
+	  flags.registerOptional('x', "exclude", String.class, "[method[,]]+", "Comma separated list of method names to ignore");
+	  flags.registerOptional('k', "inlineconstants", "Allow mutation of inline constants");
+	  flags.registerOptional('r', "returns", "Allow mutation of return values");
+	  flags.registerRequired(String.class, "class", "Class to be mutated");
+	  final Flag a1 = flags.registerRequired(Integer.class, "mutation-point", "point to mutate");
+	  final Flag a2 = flags.registerRequired(String.class, "test-class", "corresponding test class");
+	  a1.setMinCount(0);
+	  a2.setMinCount(0);
+	  flags.setValidator(new Validator() {
+	  public boolean isValid(CLIFlags flags) {
+	  if (flags.getAnonymousValue(1) != null && flags.getAnonymousValue(2) == null) {
+	  flags.setParseMessage("must give both mutation point and test class");
+	  return false;
+	  }
+	  return true;
+	  }
+	  });
+	  // following call will cause exit if problem occurs in parsing options
+	  flags.setFlags(args);
+	*/
+	// ignore set
+	
+	final HashSet ignore = new HashSet();
+
+	//get the list of excludes
+	String excludes = Utils.getOption('x', args);
+	if(excludes.equals(""))
+	    excludes = Utils.getOption("exclude", args);
+	
+	
+	if (!excludes.equals("")) {
+	    final String[] ig = excludes.split(",");
+	    for (int i = 0; i < ig.length; i++) {
+		ignore.add(ig[i]);
+	    }
+	} else {
+	    ignore.add("main");
+	    ignore.add("integrity");
+	}
+	
+	if (Utils.getFlag('c',args) || Utils.getFlag("count", args)) {
+	    final Mutater m = new Mutater(0); // run in count mode only, param ignored
+	    m.setIgnoredMethods(ignore);
+	    m.setMutateInlineConstants(Utils.getFlag('k',args) || Utils.getFlag("inlineconstants", args));
+	    m.setMutateReturnValues(Utils.getFlag('r', args) || Utils.getFlag("returns", args));
+	    System.out.println("" + m.countMutationPoints(Utils.getNextArgument(args)));
+	} else {
+	    String className = Utils.getNextArgument(args);
+	    int mutationNumber = Integer.parseInt(Utils.getNextArgument(args));
+	    String testName = Utils.getNextArgument(args);
+
+	    try {
+		final Mutater m = new Mutater(mutationNumber);
+		m.setIgnoredMethods(ignore);
+		m.setMutateInlineConstants(Utils.getFlag('k',args) || Utils.getFlag("inlineconstants", args));
+		m.setMutateReturnValues(Utils.getFlag('r', args) || Utils.getFlag("returns", args));
+		final ClassLoader loader = new Jumbler(className.replace('/', '.'), m);
+		final Class clazz = loader.loadClass("jumble.JumbleTestSuite");
+		System.err.println(clazz.getMethod("run", new Class[] { String.class }).invoke(null, new Object[] { testName.replace('/', '.') }));
+	    } catch (Exception ex) {
+		ex.printStackTrace();
+	    }
+	    
+	}
+    }
 }
---
+++ src/jumble/Utils.java
@@ -0,0 +1,397 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    Utils.java
+ *    Copyright (C) 1999-2004 University of Waikato
+ *    Originally from the Weka Project
+ *
+ */
+
+package jumble;
+
+import java.util.Vector; //I think this is better to use than Weka's FastVector here
+
+/**
+ * Class implementing simple command line parsing methods. A subset of the weka.core.Utils 
+ * class (taking all command-line related code), with some new code added.
+ *
+ * @author Tin Pavlinic
+ * The authors of weka.core.Utils are:
+
+ * @author Eibe Frank 
+ * @author Yong Wang 
+ * @author Len Trigg 
+ * @author Julien Prados
+ * @version $Revision: 1.0
+ */
+
+public final class Utils {
+
+  /**
+   * Checks if the given array contains any non-empty options.
+   *
+   * @param strings an array of strings
+   * @exception Exception if there are any non-empty options
+   */
+
+  public static void checkForRemainingOptions(String [] options) 
+    throws Exception {
+    
+    int illegalOptionsFound = 0;
+    StringBuffer text = new StringBuffer();
+
+    if (options == null) {
+      return;
+    }
+    for (int i = 0; i < options.length; i++) {
+      if (options[i].length() > 0) {
+	illegalOptionsFound++;
+	text.append(options[i] + ' ');
+      }
+    }
+    if (illegalOptionsFound > 0) {
+      throw new Exception("Illegal options: " + text);
+    }
+  }
+  
+  /**
+   * Checks if the given array contains the flag "-Char". Stops
+   * searching at the first marker "--". If the flag is found,
+   * it is replaced with the empty string.
+   *
+   * @param flag the character indicating the flag.
+   * @param strings the array of strings containing all the options.
+   * @return true if the flag was found
+   * @exception Exception if an illegal option was found
+   */
+  public static boolean getFlag(char flag, String [] options) 
+    throws Exception {
+       return getFlag("" + flag, options);
+  }
+  
+  /**
+   * Checks if the given array contains the flag "-String". Stops
+   * searching at the first marker "--". If the flag is found,
+   * it is replaced with the empty string.
+   *
+   * @param flag the String indicating the flag.
+   * @param strings the array of strings containing all the options.
+   * @return true if the flag was found
+   * @exception Exception if an illegal option was found
+   */
+  public static boolean getFlag(String flag, String [] options) 
+    throws Exception {
+
+    if (options == null) {
+      return false;
+    }
+    for (int i = 0; i < options.length; i++) {
+      if ((options[i].length() > 1) && (options[i].charAt(0) == '-')) {
+	try {
+	  Double dummy = Double.valueOf(options[i]);
+	} catch (NumberFormatException e) {
+	  if (options[i].equals("-" + flag)) {
+	    options[i] = "";
+	    return true;
+	  }
+	  if (options[i].charAt(1) == '-') {
+	    return false;
+	  }
+	}
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Gets an option indicated by a flag "-Char" from the given array
+   * of strings. Stops searching at the first marker "--". Replaces 
+   * flag and option with empty strings.
+   *
+   * @param flag the character indicating the option.
+   * @param options the array of strings containing all the options.
+   * @return the indicated option or an empty string
+   * @exception Exception if the option indicated by the flag can't be found
+   */
+    public static /*@non_null@*/ String getOption(char flag, String [] options) 
+	throws Exception {
+	return getOption("" + flag, options);
+    }
+
+  /**
+   * Gets an option indicated by a flag "-String" from the given array
+   * of strings. Stops searching at the first marker "--". Replaces 
+   * flag and option with empty strings.
+   *
+   * @param flag the String indicating the option.
+   * @param options the array of strings containing all the options.
+   * @return the indicated option or an empty string
+   * @exception Exception if the option indicated by the flag can't be found
+   */
+    public static /*@non_null@*/ String getOption(String flag, String [] options) 
+    throws Exception {
+	
+    String newString;
+
+    if (options == null)
+      return "";
+    for (int i = 0; i < options.length; i++) {
+      if ((options[i].length() > 0) && (options[i].charAt(0) == '-')) {
+	
+	// Check if it is a negative number
+	try {
+	  Double dummy = Double.valueOf(options[i]);
+	} catch (NumberFormatException e) {
+	  if (options[i].equals("-" + flag)) {
+	    if (i + 1 == options.length) {
+	      throw new Exception("No value given for -" + flag + " option.");
+	    }
+	    options[i] = "";
+	    newString = new String(options[i + 1]);
+	    options[i + 1] = "";
+	    return newString;
+	  }
+	  if (options[i].charAt(1) == '-') {
+	    return "";
+	  }
+	}
+      }
+    }
+    return "";
+  }
+
+  /**
+   * Returns the secondary set of options (if any) contained in
+   * the supplied options array. The secondary set is defined to
+   * be any options after the first "--". These options are removed from
+   * the original options array.
+   *
+   * @param options the input array of options
+   * @return the array of secondary options
+   */
+  public static String [] partitionOptions(String [] options) {
+
+    for (int i = 0; i < options.length; i++) {
+      if (options[i].equals("--")) {
+	options[i++] = "";
+	String [] result = new String [options.length - i];
+	for (int j = i; j < options.length; j++) {
+	  result[j - i] = options[j];
+	  options[j] = "";
+	}
+	return result;
+      }
+    }
+    return new String [0];
+  }
+    
+ 
+  /**
+   * Split up a string containing options into an array of strings,
+   * one for each option.
+   *
+   * @param optionString the string containing the options
+   * @return the array of options
+   */
+  public static String [] splitOptions(String quotedOptionString) throws Exception{
+
+    Vector optionsVec = new Vector();
+    String str = new String(quotedOptionString);
+    int i;
+    
+    while (true){
+
+      //trimLeft 
+      i = 0;
+      while ((i < str.length()) && (Character.isWhitespace(str.charAt(i)))) i++;
+      str = str.substring(i);
+      
+      //stop when str is empty
+      if (str.length() == 0) break;
+      
+      //if str start with a double quote
+      if (str.charAt(0) == '"'){
+	
+	//find the first not anti-slached double quote
+	i = 1;
+	while(i < str.length()){
+	  if (str.charAt(i) == str.charAt(0)) break;
+	  if (str.charAt(i) == '\\'){
+	    i += 1;
+	    if (i >= str.length()) 
+	      throw new Exception("String should not finish with \\");
+	    if (str.charAt(i) != '\\' &&  str.charAt(i) != '"') 
+	      throw new Exception("Unknow character \\" + str.charAt(i));
+	  }
+	  i += 1;
+	}
+	if (i >= str.length()) throw new Exception("Quote parse error.");
+	
+	//add the founded string to the option vector (without quotes)
+	String optStr = str.substring(1,i);
+	optStr = unbackQuoteChars(optStr);
+	optionsVec.addElement(optStr);
+	str = str.substring(i+1);
+      } else {
+	//find first whiteSpace
+	i=0;
+	while((i < str.length()) && (!Character.isWhitespace(str.charAt(i)))) i++;
+	
+	//add the founded string to the option vector
+	String optStr = str.substring(0,i);
+	optionsVec.addElement(optStr);
+	str = str.substring(i);
+      }
+    }
+    
+    //convert optionsVec to an array of String
+    String [] options = new String[optionsVec.size()];
+    for (i = 0; i < optionsVec.size(); i++) {
+      options[i] = (String)optionsVec.elementAt(i);
+    }
+    return options;
+  }    
+
+    /** Get the next non-empty argument in the arguments list.
+     *  The returned argument is cleared to the empty string in the args
+     * array.
+     * @param args the arguments array
+     * @return the first non-empty string in the array or the empty string if 
+     *         all strings are empty
+     */
+
+    public static String getNextArgument(String [] args) {
+	for(int i = 0; i < args.length; i++) {
+	    if(!args[i].equals("")) {
+		String retVal = args[i];
+		args[i] = "";
+		return retVal;
+	    }
+	}
+	return "";
+    }
+
+  /**
+   * Joins all the options in an option array into a single string,
+   * as might be used on the command line.
+   *
+   * @param optionArray the array of options
+   * @return the string containing all options.
+   */
+  public static String joinOptions(String [] optionArray) {
+
+    String optionString = "";
+    for (int i = 0; i < optionArray.length; i++) {
+      if (optionArray[i].equals("")) {
+	continue;
+      }
+      if (optionArray[i].indexOf(' ') != -1) {
+	optionString += '"' + optionArray[i] + '"';
+      } else {
+	optionString += optionArray[i];
+      }
+      optionString += " ";
+    }
+    return optionString.trim();
+  }
+
+    /**
+     * The inverse operation of backQuoteChars().
+     * Converts back-quoted carriage returns and new lines in a string 
+     * to the corresponding character ('\r' and '\n').
+     * Also "un"-back-quotes the following characters: ` " \ \t and %
+     * @param string the string
+     * @return the converted string
+     */
+    public static String unbackQuoteChars(String string) {
+	
+	int index;
+	StringBuffer newStringBuffer;
+    
+	// replace each of the following characters with the backquoted version
+	String charsFind[]    = {"\\\\", "\\'", "\\t", "\\\"", "\\%"};
+	char   charsReplace[] = {'\\',   '\'',  '\t',  '"',    '%'};
+	
+	for(int i = 0; i < charsFind.length; i++) {
+	    if (string.indexOf(charsFind[i]) != -1 ) {
+		newStringBuffer = new StringBuffer();
+		while ((index = string.indexOf(charsFind[i])) != -1) {
+		    if (index > 0) {
+			newStringBuffer.append(string.substring(0, index));
+		    }
+		    newStringBuffer.append(charsReplace[i]);
+		    if ((index + charsFind[i].length()) < string.length()) {
+			string = string.substring(index + charsFind[i].length());
+		    } else {
+			string = "";
+		    }
+		}
+		newStringBuffer.append(string);
+		string = newStringBuffer.toString();
+	    }
+	}
+	return Utils.convertNewLines(string);
+    }    
+    
+    /**
+     * Converts carriage returns and new lines in a string into \r and \n.
+     * @param string the string
+     * @return the converted string
+     */
+    public static /*@pure@*/ String convertNewLines(String string) {
+	
+	int index;
+
+    // Replace with \n
+    StringBuffer newStringBuffer = new StringBuffer();
+    while ((index = string.indexOf('\n')) != -1) {
+      if (index > 0) {
+	newStringBuffer.append(string.substring(0, index));
+      }
+      newStringBuffer.append('\\');
+      newStringBuffer.append('n');
+      if ((index + 1) < string.length()) {
+	string = string.substring(index + 1);
+      } else {
+	string = "";
+      }
+    }
+    newStringBuffer.append(string);
+    string = newStringBuffer.toString();
+    
+    // Replace with \r
+    newStringBuffer = new StringBuffer();
+    while ((index = string.indexOf('\r')) != -1) {
+      if (index > 0) {
+	newStringBuffer.append(string.substring(0, index));
+      }
+      newStringBuffer.append('\\');
+      newStringBuffer.append('r');
+      if ((index + 1) < string.length()){
+	string = string.substring(index + 1);
+      } else {
+	string = "";
+      }
+    }
+    newStringBuffer.append(string);
+    return newStringBuffer.toString();
+  }
+    
+
+
+}
+  
