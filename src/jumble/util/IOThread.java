eb0992c633bbf652caf10f5b82f62c043887258c
sean <sean@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2005-11-15 03:28:28 +0000

    style
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@63 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- src/jumble/util/IOThread.java
+++ src/jumble/util/IOThread.java
@@ -4,47 +4,50 @@ import java.util.List;
 import java.util.LinkedList;
 
 import java.io.*;
-/** Class which constantly polls an InputStream and allows the user
+
+/**
+ * Class which constantly polls an InputStream and allows the user
  * to see if any data is available.
+ *
  * @author Tin Pavlinic
  */
 public class IOThread extends Thread {
-    public final static boolean DEBUG = false;
-       private BufferedReader mOut;
-       private List mBuffer;
-       //Constructor
-       public IOThread(InputStream out) {
-           mOut = new BufferedReader(new InputStreamReader(out));
-           mBuffer = new LinkedList();
-       }
-       /** Loops while the stream exists.*/
-       public void run() {
-           String curLine;
-           try {
-               while((curLine = mOut.readLine())!= null) {  
-                 synchronized(this) {
-                       mBuffer.add(0, curLine);
-                   }
-               }
-               if(DEBUG) {
-                   System.out.println("F");
-               }
-           } catch(IOException e) {
-               //e.printStackTrace();
-               return;
-           }
-       }
-       /** Returns the next line of text if available. Otherwise
-        * returns null.
-        * @return the next line of text or null
-        */
-       public String getNext() {
-           synchronized(this) {
-               if(mBuffer.size() == 0)
-                   return null;
-               String ret = (String)mBuffer.get(mBuffer.size() - 1);
-               mBuffer.remove(mBuffer.size() - 1);
-               return ret;
-           }
-       }
-   }
+  public final static boolean DEBUG = false;
+  private BufferedReader mOut;
+  private List mBuffer;
+  //Constructor
+  public IOThread(InputStream out) {
+    mOut = new BufferedReader(new InputStreamReader(out));
+    mBuffer = new LinkedList();
+  }
+  /** Loops while the stream exists.*/
+  public void run() {
+    String curLine;
+    try {
+      while((curLine = mOut.readLine())!= null) {  
+        synchronized(this) {
+          mBuffer.add(0, curLine);
+        }
+      }
+      if(DEBUG) {
+        System.out.println("F");
+      }
+    } catch(IOException e) {
+      //e.printStackTrace();
+      return;
+    }
+  }
+  /** Returns the next line of text if available. Otherwise
+   * returns null.
+   * @return the next line of text or null
+   */
+  public String getNext() {
+    synchronized(this) {
+      if(mBuffer.size() == 0)
+        return null;
+      String ret = (String)mBuffer.get(mBuffer.size() - 1);
+      mBuffer.remove(mBuffer.size() - 1);
+      return ret;
+    }
+  }
+}
