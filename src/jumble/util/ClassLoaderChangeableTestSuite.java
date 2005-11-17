084dc569f8406069225b82401806b26c27ae407f
sean <sean@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2005-11-17 21:24:26 +0000

    REC
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@105 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- src/jumble/util/ClassLoaderChangeableTestSuite.java
+++ src/jumble/util/ClassLoaderChangeableTestSuite.java
@@ -3,14 +3,15 @@ package jumble.util;
 import java.lang.reflect.Constructor;
 
 import junit.framework.TestSuite;
+import java.lang.reflect.InvocationTargetException;
 /**
  * A test suite which is capable of being loaded in a new class loader.
  * @author Tin Pavlinic
  * @version $Revision$
  *
  */
-public class ClassLoaderChangeableTestSuite extends TestSuite implements
-    ClassLoaderChanger {
+public class ClassLoaderChangeableTestSuite extends TestSuite implements ClassLoaderChanger {
+
   public ClassLoaderChangeableTestSuite(String className) throws ClassNotFoundException {
     super(Class.forName(className));
   }
@@ -22,7 +23,13 @@ public class ClassLoaderChangeableTestSuite extends TestSuite implements
       Constructor con = clazz.getConstructor(new Class[] {String.class});
 
       return con.newInstance(new Object[] {getName()});
-    } catch (Exception e) {
+    } catch (InvocationTargetException e) {
+      throw new ClassNotFoundException(e.getMessage());
+    } catch (InstantiationException e) {
+      throw new ClassNotFoundException(e.getMessage());
+    } catch (IllegalAccessException e) {
+      throw new ClassNotFoundException(e.getMessage());
+    } catch (NoSuchMethodException e) {
       throw new ClassNotFoundException(e.getMessage());
     }
   }
