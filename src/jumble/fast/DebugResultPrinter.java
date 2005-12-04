50b855022bd3e82863eeedbbdbd94b94250aa2fb
sean <sean@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2005-12-04 21:00:48 +0000

    tidy
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@138 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- src/jumble/fast/DebugResultPrinter.java
+++ src/jumble/fast/DebugResultPrinter.java
@@ -1,9 +1,3 @@
-/*
- * Created on 7/07/2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package jumble.fast;
 
 import java.io.PrintStream;
