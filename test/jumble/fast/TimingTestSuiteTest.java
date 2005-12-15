0689740a132ab71b2cce081987b13245c905137a
len <len@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2005-12-15 21:47:44 +0000

    Add javadoc.
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@211 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- test/jumble/fast/TimingTestSuiteTest.java
+++ test/jumble/fast/TimingTestSuiteTest.java
@@ -19,6 +19,10 @@ public class TimingTestSuiteTest extends TestCase {
   private static final int MED_DELAY = 500;
   private static final int LONG_DELAY = 2000;
 
+  /**
+   * <code>TimedTests</code> is a small set of tests that take varying
+   * times to execute.
+   */
   public static class TimedTests extends TestCase {
     // Warning, the declaration order of these tests is important to testGetOrder below
     public final void testMedium() throws Exception {
