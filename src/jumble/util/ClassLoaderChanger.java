65cd3a11e0100f2ea0e7f48a43dcd73f28d70259
len <len@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2005-12-18 22:11:34 +0000

    Docs.
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@234 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- src/jumble/util/ClassLoaderChanger.java
+++ src/jumble/util/ClassLoaderChanger.java
@@ -1,22 +1,23 @@
 package jumble.util;
 
 /**
- * Interface for classes which can be dynamically reloaded and cloned in a new
+ * Interface for objects that can be dynamically cloned under a new
  * class loader.
  * 
  * @author Tin Pavlinic
  * @version $Revision$
  */
 public interface ClassLoaderChanger {
+
   /**
-   * Reloads the class in the specified class loader and clones the object.
+   * Returns a clone of the object as loaded by the specified class
+   * loader.
    * 
-   * @param loader
-   *          the new class loader to load in
+   * @param loader the new class loader to load in
    * @return a clone of the object in the new class loader
-   * @throws ClassNotFoundException
-   *           if <CODE>loader</CODE> could not load the class or if the class
-   *           cannot be cloned in the new loader.
+   * @throws ClassNotFoundException if <CODE>loader</CODE> could not
+   * load the class or if the class cannot be cloned in the new
+   * loader.
    */
   Object changeClassLoader(ClassLoader loader) throws ClassNotFoundException;
 }
