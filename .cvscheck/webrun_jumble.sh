04376c83f70e1936e22ab03cf117323602e7486d
len <len@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2005-12-23 02:37:56 +0000

    Smarter env settings.
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@258 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- .cvscheck/webrun_jumble.sh
+++ .cvscheck/webrun_jumble.sh
@@ -1,19 +1,30 @@
 #! /bin/sh
 
-export HOME=/home/syscheck
+LOGNAME=${LOGNAME:-$USER}
+if [ ! "$HOME" ]; then
+    export HOME=/home/$LOGNAME
+fi
+
 export JAVA=/usr/local/java/jdk
-export PATH=$PATH:$JAVA/bin:$HOME/bin:/usr/local/bin
-export CVSROOT=:pserver:syscheck@giger:/home/cvs-repository2
-
-export WEBROOT=/share/reeltwo/html/keachecker
-export SCRIPTS=$WEBROOT/cvscheck
 export SYSNAME=cvscheck_jumble
-export CODEHOME=$WEBROOT/${SYSNAME}_java
-export WEB=$WEBROOT/html/${SYSNAME}
-export EMAIL_NOTIFY=1
 
+
+if [ "$LOGNAME" == "syscheck" ]; then
+    export WEBROOT=/share/reeltwo/html/keachecker
+    export SCRIPTS=$WEBROOT/cvscheck
+    export CODEHOME=$WEBROOT/${SYSNAME}_java
+    export WEB=$WEBROOT/html/${SYSNAME}
+    export EMAIL_NOTIFY=1
+else
+    export CODEHOME=$HOME/reeltwo_sandboxes
+fi
 
 export MAIN_MODULE="jumble"
+if [ -f "$CODEHOME/$MAIN_MODULE/CVS/Root" ]; then
+    export CVSROOT=$(cat "$CODEHOME/$MAIN_MODULE/CVS/Root")
+else
+    export CVSROOT=:pserver:$LOGNAME@giger:/home/cvs-repository2
+fi
 export SRC_MODULES="jumble/src"
 export LIB_MODULES="jumble/lib"
 export TEST_MODULES="jumble/test"
@@ -26,6 +37,5 @@ export DISABLE_JUMBLE=1    # Don't run jumble on itself
 (
     (sh $SCRIPTS/cvscheck.sh cvscheck_main 2>&1)
 ) >/dev/null
-
 
 
