3d9ec4c18ecf6c89382c90b6ce04490b09017c15
len <len@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2006-01-04 23:01:27 +0000

    Enable IM, and add comments.
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@277 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- .cvscheck/webrun_jumble.sh
+++ .cvscheck/webrun_jumble.sh
@@ -5,20 +5,21 @@ if [ ! "$HOME" ]; then
     export HOME=/home/$LOGNAME
 fi
 
-export JAVA=/usr/local/java/jdk
+export JAVA_HOME=/usr/local/java/jdk
 export SYSNAME=cvscheck_jumble
 
 
+# Set variables for where to check out to and where to put results
 if [ "$LOGNAME" == "syscheck" ]; then
     export WEBROOT=/share/reeltwo/html/keachecker
     export SCRIPTS=$WEBROOT/cvscheck
     export CODEHOME=$WEBROOT/${SYSNAME}_java
     export WEB=$WEBROOT/html/${SYSNAME}
-    export EMAIL_NOTIFY=1
 else
     export CODEHOME=$HOME/reeltwo_sandboxes
 fi
 
+# Description of modules in CVS
 export MAIN_MODULE="jumble"
 if [ -f "$CODEHOME/$MAIN_MODULE/CVS/Root" ]; then
     export CVSROOT=$(cat "$CODEHOME/$MAIN_MODULE/CVS/Root")
@@ -30,7 +31,13 @@ export LIB_MODULES="jumble/lib"
 export TEST_MODULES="jumble/test"
 export DOC_MODULES=" "
 export JAVADOC_MODULES="jumble/src"
+
+# Options controlling how things get run
 export JIKES_OPTS="+E +P -source 1.4 -deprecation"
+if [ "$USER" == "syscheck" ]; then
+    export EMAIL_NOTIFY=1
+    export IM_NOTIFY=1
+fi
 export LEVEL=2
 export DISABLE_JUMBLE=1    # Don't run jumble on itself
 
