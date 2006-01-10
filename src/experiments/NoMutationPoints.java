39b17942b9b9b9f20f4a751dd2607be2f8a93357
len <len@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2006-01-10 03:01:47 +0000

    New files
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@293 3c96dea9-a453-4bd5-b869-110af7cb2dd7

---
+++ src/experiments/NoMutationPoints.java
@@ -0,0 +1,10 @@
+package experiments;
+
+/**
+ * Class to demonstrate a class with no mutation points.
+ * 
+ * @author Tin Pavlinic
+ * @version $Revision 1.0 $
+ */
+public class NoMutationPoints {
+}
---
+++ src/experiments/NoMutationPointsTest.java
@@ -0,0 +1,14 @@
+package experiments;
+
+import junit.framework.TestCase;
+
+/**
+ * NoMutationPoints test class.
+ * 
+ * @author Tin Pavlinic
+ * @version $Revision 1.0 $
+ */
+public class NoMutationPointsTest extends TestCase {
+  public void testNothing() {
+  }
+}
