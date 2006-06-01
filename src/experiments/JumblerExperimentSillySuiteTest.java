23deb4b64a17915d760f3a604efa155cfc6de6a7
tin <tin@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2006-06-01 23:05:21 +0000

    javadoc
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@336 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- src/experiments/JumblerExperimentSillySuiteTest.java
+++ src/experiments/JumblerExperimentSillySuiteTest.java
@@ -3,6 +3,12 @@ package experiments;
 import junit.framework.Test;
 import junit.framework.TestCase;
 
+/**
+ * Test class used for testing Junble's use of the suite() method.
+ * 
+ * @author Tin Pavlinic
+ * @version $Revision 1.0 $
+ */
 public class JumblerExperimentSillySuiteTest extends TestCase {
   public static Test suite() {
     return JumblerExperimentTest.suite();
