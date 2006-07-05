5624064cc579e929d106dd994f17458e6edd2111
tin <tin@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2006-07-05 23:18:08 +0000

    mutating LOADs
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@371 3c96dea9-a453-4bd5-b869-110af7cb2dd7

---
+++ src/experiments/instruction/Assert.java
@@ -0,0 +1,14 @@
+package experiments.instruction;
+
+/**
+ * Class for Jumble testing.
+ * 
+ * @author Tin Pavlinic
+ * @version $Revision 1.0 $
+ */
+public class Assert {
+  public void method() {
+    int x = 1;
+    assert x == 1;
+  }
+}
---
+++ src/experiments/instruction/DLoad.java
@@ -0,0 +1,11 @@
+package experiments.instruction;
+/**
+ * Class used for testing the mutater.
+ * @author Tin Pavlinic
+ * @version $Revision 1.0 $
+ */
+public class DLoad {
+  public double method(double d0, double d1, double d2, double d3, double d4, double d5) {
+    return d5;
+  }
+}
