98d460fdaa07d566d72a58cea94cb16dd8732873
lenbok <lenbok@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2007-04-16 00:20:58 +0000

    Removed scripts directory (unused)
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@507 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp *.gif update.sh update-subset.sh key.html dep_* /share/reeltwo/html/jumble
-chmod 0666 /share/reeltwo/html/jumble/{*.gif,update.sh,key.html}
-chmod u+x /share/reeltwo/html/jumble/update.sh
