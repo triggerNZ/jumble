2c0d91a47e568b3f31f557cb4f24eabd40af7552
marku <marku@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2007-06-06 05:55:28 +0000

    MODIFY: add .css file and jumble icon. Give warning and instructions about changing file permissions.
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@525 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- web/UpdateWeb.sh
+++ web/UpdateWeb.sh
@@ -7,5 +7,11 @@ fi
 USER=$1
 WEBSITE=shell.sourceforge.net:/home/groups/j/ju/jumble/htdocs
 shift
-echo "Copying *.html *.gif to $USER@$WEBSITE"
-scp *.html *.gif $USER@$WEBSITE
+echo "Copying *.html *.gif *.css jumble_icon.png to $USER@$WEBSITE"
+scp *.html *.gif *.css jumble_icon.png $USER@$WEBSITE
+
+echo "NOTE: you may need to correct the permissions of any new files"
+echo "Eg. ssh shell.sourceforge.net"
+echo "    cd /home/groups/j/ju/jumble/htdocs"
+echo "    chmod 664 *.*"
+
