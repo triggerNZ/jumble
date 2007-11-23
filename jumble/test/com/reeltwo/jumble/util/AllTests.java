832f008e4e24db6b3e73992f1bcdb3f1963d7f6c
tp20 <tp20@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2007-11-23 10:23:51 +0000

    Removed the ancient unused RTSI stuff
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@562 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- jumble/test/com/reeltwo/jumble/util/AllTests.java
+++ jumble/test/com/reeltwo/jumble/util/AllTests.java
@@ -13,10 +13,8 @@ public class AllTests extends TestSuite {
 
   public static Test suite() {
     TestSuite suite = new TestSuite();
-    suite.addTest(BCELRTSITest.suite());
     suite.addTest(IOThreadTest.suite());
     suite.addTest(JavaRunnerTest.suite());
-    suite.addTest(RTSITest.suite());
     suite.addTest(JumbleUtilsTest.suite());
     suite.addTest(JUnit4Test.suite());
     return suite;
