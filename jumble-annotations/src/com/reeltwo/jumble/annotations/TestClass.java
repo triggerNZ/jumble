7cb081b38fc70bb53e1d90266d973bb294e0f76d
tp20 <tp20@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2008-05-26 10:52:22 +0000

    Initial import of Jumble annotations project
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@583 3c96dea9-a453-4bd5-b869-110af7cb2dd7

---
+++ jumble-annotations/src/com/reeltwo/jumble/annotations/TestClass.java
@@ -0,0 +1,12 @@
+package com.reeltwo.jumble.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface TestClass {
+  String[] value();
+}
