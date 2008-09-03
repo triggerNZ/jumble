c4b5279d30333c105bacd4aae476ddf2b848395e
sairvin <sairvin@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2008-09-03 22:10:25 +0000

    fix docs
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@590 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- jumble/src/experiments/Enum1Test.java
+++ jumble/src/experiments/Enum1Test.java
@@ -1,10 +1,11 @@
 package experiments;
 
 import junit.framework.TestCase;
+
 /**
- * Enum test used for testing
+ * Enum test used for testing.
  * @author Tin Pavlinic
- * @version $Revision $
+ * @version $Revision: $
  */
 public class Enum1Test extends TestCase {
   public void test() {
