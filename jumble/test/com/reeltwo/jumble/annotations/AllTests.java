3d31bc21496028b8189ff26d0f19620e4c140573
sairvin <sairvin@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2008-09-03 22:18:26 +0000

    link
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@595 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- jumble/test/com/reeltwo/jumble/annotations/AllTests.java
+++ jumble/test/com/reeltwo/jumble/annotations/AllTests.java
@@ -5,19 +5,19 @@ import junit.framework.TestSuite;
 import junit.textui.TestRunner;
 
 /**
- * Runs all com.reeltwo.jumble tests.
+ * Runs tests.
  *
  * @author <a href="mailto:len@reeltwo.com">Len Trigg</a>
  * @version $Revision: 576 $
  */
 public class AllTests extends TestSuite {
   public static Test suite() {
-    TestSuite suite = new TestSuite();
+    final TestSuite suite = new TestSuite();
     suite.addTest(JumbleAnnotationProcessorTest.suite());
     return suite;
   }
   
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     TestRunner.run(suite());
   }
 }
