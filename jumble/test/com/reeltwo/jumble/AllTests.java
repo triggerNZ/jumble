af7e5ce9c8a8b8de6a1216125720afd3f9851078
sairvin <sairvin@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2008-09-03 22:19:25 +0000

    tidy up
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@596 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- jumble/test/com/reeltwo/jumble/AllTests.java
+++ jumble/test/com/reeltwo/jumble/AllTests.java
@@ -12,19 +12,17 @@ import junit.textui.TestRunner;
  */
 public class AllTests extends TestSuite {
   public static Test suite() {
-    TestSuite suite = new TestSuite();
-
+    final TestSuite suite = new TestSuite();
     suite.addTest(JumbleTest.suite());
-    
-    suite.addTest(com.reeltwo.jumble.util.AllTests.suite());
-    suite.addTest(com.reeltwo.jumble.mutation.AllTests.suite());
-    suite.addTest(com.reeltwo.jumble.fast.AllTests.suite());
-    suite.addTest(com.reeltwo.jumble.ui.AllTests.suite());
     suite.addTest(com.reeltwo.jumble.annotations.AllTests.suite());
+    suite.addTest(com.reeltwo.jumble.fast.AllTests.suite());
+    suite.addTest(com.reeltwo.jumble.mutation.AllTests.suite());
+    suite.addTest(com.reeltwo.jumble.ui.AllTests.suite());
+    suite.addTest(com.reeltwo.jumble.util.AllTests.suite());
     return suite;
   }
   
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     TestRunner.run(suite());
   }
 }
