07c2032fb720d6ba24624082a170295ee9c18721
sairvin <sairvin@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2008-09-05 02:19:40 +0000

    style
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@613 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- jumble/src/com/reeltwo/jumble/annotations/JumbleAnnotationProcessor.java
+++ jumble/src/com/reeltwo/jumble/annotations/JumbleAnnotationProcessor.java
@@ -12,15 +12,15 @@ import com.reeltwo.jumble.mutation.MutatingClassLoader;
  * @version $Revision: $
  */
 public class JumbleAnnotationProcessor {
-  public List < String > getTestClassNames(String className) throws ClassNotFoundException {
+  public List<String> getTestClassNames(String className) throws ClassNotFoundException {
     //Load in a different class loader to prevent the initial loading of the class from affecting the jumble run
     final ClassLoader oldLoader = Thread.currentThread().getContextClassLoader();
     try {
       MutatingClassLoader jumbler = new MutatingClassLoader(className, new Mutater(-1), System.getProperty("java.class.path"));
       Thread.currentThread().setContextClassLoader(jumbler);
-      Class < ? > clazz = Class.forName(className);
+      Class<?> clazz = Class.forName(className);
       TestClass testClass = clazz.getAnnotation(TestClass.class);
-      List < String > testClassNames = new ArrayList < String > ();
+      List<String> testClassNames = new ArrayList<String>();
       if (testClass != null) {
         String[] testClasses = testClass.value();
         for (String testClassName : testClasses) {
