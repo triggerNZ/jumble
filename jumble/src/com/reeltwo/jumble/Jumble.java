c4e4007cda9e8008fd24d2dab4444b2dfdd9b82e
lenbok <lenbok@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2008-11-27 21:47:30 +0000

    Style.
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@659 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- jumble/src/com/reeltwo/jumble/Jumble.java
+++ jumble/src/com/reeltwo/jumble/Jumble.java
@@ -3,7 +3,6 @@ package com.reeltwo.jumble;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import com.reeltwo.jumble.fast.FastRunner;
