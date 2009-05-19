c58596c05505c466a91147b717ace22a12e21623
lenbok <lenbok@3c96dea9-a453-4bd5-b869-110af7cb2dd7>
2009-05-19 01:07:59 +0000

    Updated logo
    git-svn-id: https://jumble.svn.sourceforge.net/svnroot/jumble/trunk@676 3c96dea9-a453-4bd5-b869-110af7cb2dd7

--- web/jumble.js
+++ web/jumble.js
@@ -2,10 +2,10 @@
 <!--
   var aryImages = new Array(12);
 
-  for (x=0; x<4; x++) {
+  for (x=0; x<3; x++) {
       for (y=0; y<3; y++) {
-          i = y * 4 + x;
-          aryImages[i] = "r2."+x+"."+y+".png";
+          i = y * 3 + x;
+          aryImages[i] = "logopart."+x+"."+y+".png";
       }
   }
   aryImages[3] = "blank.png";
@@ -23,20 +23,20 @@
       rand = Math.random();
       index1 = -1;
       if (rand < 0.25) {
-        if (bIndex % 4 != 3) {
+        if (bIndex % 3 != 2) {
           index1 = bIndex + 1;
         }
       } else if (rand < 0.5) {
-        if (bIndex % 4 != 0) {
+        if (bIndex % 3 != 0) {
           index1 = bIndex - 1;
         }
       } else if (rand < 0.75) {
-        if (bIndex < 8) {
-          index1 = bIndex + 4;
+        if (bIndex < 6) {
+          index1 = bIndex + 3;
         }
       } else {
-        if (bIndex > 3) {
-          index1 = bIndex - 4;
+        if (bIndex > 2) {
+          index1 = bIndex - 3;
         }
       }
 
@@ -45,8 +45,8 @@
         tmp = aryImages[index2];
         aryImages[index2] = aryImages[index1];
         aryImages[index1] = tmp;
-        document['rt'+index1].src = aryImages[index1];
-        document['rt'+index2].src = aryImages[index2];
+        document['logopart'+index1].src = aryImages[index1];
+        document['logopart'+index2].src = aryImages[index2];
 
         prev = bIndex;
         bIndex = index1;
@@ -59,22 +59,19 @@ swap_timer = setInterval('swap();', 333);
 </SCRIPT>
 <table cellpadding=0 cellspacing=1 bgcolor="#dddddd" border=0>
   <tr>
-    <td><img name="rt0" src="r2.0.0.png" border=0></td>
-    <td><img name="rt1" src="r2.1.0.png" border=0></td>
-    <td><img name="rt2" src="r2.2.0.png" border=0></td>
-    <td><img name="rt3" src="blank.png" border=0></td>
+    <td><img name="logopart0" src="logopart.0.0.png" border=0></td>
+    <td><img name="logopart1" src="logopart.1.0.png" border=0></td>
+    <td><img name="logopart2" src="blank.png" border=0></td>
   </tr>
   <tr>
-    <td><img name="rt4" src="r2.0.1.png" border=0></td>
-    <td><img name="rt5" src="r2.1.1.png" border=0></td>
-    <td><img name="rt6" src="r2.2.1.png" border=0></td>
-    <td><img name="rt7" src="r2.3.1.png" border=0></td>
+    <td><img name="logopart3" src="logopart.0.1.png" border=0></td>
+    <td><img name="logopart4" src="logopart.1.1.png" border=0></td>
+    <td><img name="logopart5" src="logopart.2.1.png" border=0></td>
   </tr>
   <tr>
-    <td><img name="rt8" src="r2.0.2.png" border=0></td>
-    <td><img name="rt9" src="r2.1.2.png" border=0></td>
-    <td><img name="rt10" src="r2.2.2.png" border=0></td>
-    <td><img name="rt11" src="r2.3.2.png" border=0></td>
+    <td><img name="logopart6" src="logopart.0.2.png" border=0></td>
+    <td><img name="logopart7" src="logopart.1.2.png" border=0></td>
+    <td><img name="logopart8" src="logopart.2.2.png" border=0></td>
   </tr>
 </table>
 
